e98888f6bcb246bc185197a06c66cbf2b5436f33
Ariel Marcelo <55715664+Ariel-Marcelo@users.noreply.github.com>
2025-02-07 08:59:36 -0500

    feature: improve web site

--- code-hub/src/app/shared/data/Projects.ts
+++ code-hub/src/app/shared/data/Projects.ts
@@ -2,9 +2,11 @@ import {Project} from "../model/Project";
 
 export const projects: Project[] = [
   {
-    title: 'E-commerce Platform',
-    description: 'A full-stack e-commerce solution with real-time inventory management and secure payment processing.',
-    technologies: 'Angular, Node.js, MongoDB, Docker'
+    title: 'Calculadora de Salarios en Ecuador',
+    description: 'Web estática de Azure que consume Azure Functions, arquitectura altamente escalable para estimar las ganancias recibidas producto de un tipo de contrato específico en el Ecuador',
+    technologies: 'Vue.js, .Net Core 8, Azure, Github Actions',
+    code: 'https://github.com/Ariel-Marcelo/AzureSalaryCalculatorVue',
+    demo: 'https://calm-beach-04c94340f.4.azurestaticapps.net'
   },
 ];
 
--- code-hub/src/app/shared/model/Project.ts
+++ code-hub/src/app/shared/model/Project.ts
@@ -2,4 +2,6 @@ export interface Project {
   title: string;
   description: string;
   technologies: string;
+  code: string;
+  demo: string;
 }
